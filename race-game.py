773f456ba3abbf77da975770f9242b0bdb6e2ff5
lvxq37-2 <130481780+lvxq37-2@users.noreply.github.com>
2023-04-24 00:12:51 +0100

    Turtles move forward based on corresponding (valid) dice roll

--- race-game.py
+++ race-game.py
@@ -26,15 +26,35 @@ p1 = Turtle()
 p1.shape('turtle')
 p1.color('yellow')
 p1.penup()
-p1.goto(-250, 100)
+p1.goto(-260, 100)
 
 p2 = Turtle()
 p2.shape('turtle')
 p2.color('blue')
 p2.penup()
-p2.goto(-250, -100)
+p2.goto(-260, -100)
 
 # Dice
-dice = [1, 2, 3, 4, 5, 6]
+dice_val = [1, 2, 3, 4, 5, 6]
+
+# Overall, players need to move 480 pixels to win.
+# A score of 1 will move the player forward by 22 pixels 
+# This amounts to a maximum of 20 moves or minimum of 4 in order for a player to win
+
+for i in range(20):
+    if p1.pos() > (220, 100):
+        pass
+    elif p2.pos() > (220, -100):
+        pass
+    else:
+        p1_roll = int(input("Please enter the number you rolled on your die: "))
+        if p1_roll in dice_val:
+            print("Player 1 moves forward", p1_roll, "steps")
+            p1.forward(22*p1_roll)
+        p2_roll = int(input("Please enter the number you rolled on your die: "))
+        if p2_roll in dice_val:
+            print("Player 2 moves forward", p2_roll, "steps")
+            p2.forward(22*p2_roll)
+        
 
 done()
